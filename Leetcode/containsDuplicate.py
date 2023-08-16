359144f153d001bd1232ebb1982fa5dd79ed7f57
abhik <aavikhore@gmail.com>
2023-08-16 12:08:35 +0530

    removed duplicate files

--- Leetcode/containsDuplicate.py
+++
@@ -1,8 +0,0 @@
-def containsDuplicate(nums):
-    if len(set(nums)) == len(nums):
-        return False
-    else:
-        return True
-# nums = [1,2,3,1]
-nums = [1,2,3,4]
-print(containsDuplicate(nums=nums))
